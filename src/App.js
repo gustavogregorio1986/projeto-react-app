a33846705fcb19d5b7ad25a985fac7e8a223e01d
Miguel Marquiori <miguelmarkiori@outlook.com>
2026-03-25 19:05:27 -0300

    Desenvolver soma e imagem no projeto

--- src/App.js
+++ src/App.js
@@ -2,10 +2,23 @@
 import './App.css';
 
 function App() {
+   const name = 'Matheus'  
+   const newName = name.toUpperCase()
+
+   function sum(a, b){
+    return a + b
+   }
+
+   const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-       <h1>Ola react!</h1>
-       <p>Meu primeiro App</p>
+       <h2>Alterando JSX</h2>
+       <p>Olá, {name}</p>
+       <p>Olá, {newName}</p>
+       <p>Soma: {2 + 2}</p>
+       <p>Soma: {sum(1,2)}</p>
+       <img src={url} alt="Minha Imagem" />
     </div>
   );
 }
